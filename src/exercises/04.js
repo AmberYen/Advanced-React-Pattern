925bbda9086e0f7f835645eb7d81871cc01c2ed4
Stanney Yen <freehourse2sh@gmail.com>
2021-04-26 00:32:01 +0800

    finished exercise  04 & 05

--- src/exercises/04.js
+++ src/exercises/04.js
@@ -16,18 +16,10 @@ class Toggle extends React.Component {
     )
   render() {
     const {on} = this.state
-    // We want to give rendering flexibility, so we'll be making
-    // a change to our render prop component here.
-    // You'll notice the children prop in the Usage component
-    // is a function. 🐨 So you can replace this with a call this.props.children()
-    // But you'll need to pass it an object with `on` and `toggle`.
-    return <Switch on={on} onClick={this.toggle} />
+    return this.props.children({on, toggle: this.toggle});
   }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
 function Usage({
   onToggle = (...args) => console.log('onToggle', ...args),
 }) {
